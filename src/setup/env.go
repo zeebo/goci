5c80807df37bc975ab46b6374e108a8fe4f69c36
jeff <leterip@gmail.com>
2012-05-25 12:59:22 -0400

    changed to use APPROOT env var

--- src/setup/env.go
+++ src/setup/env.go
@@ -3,7 +3,7 @@ package setup
 import fp "path/filepath"
 
 var (
-	appdir  = env("APPDIR", ".")
+	appdir  = env("APPROOT", ".")
 	DISTDIR = fp.Join(appdir, "dist")
 	VENVDIR = fp.Join(appdir, "venv")
 	GOROOT  = fp.Join(appdir, "go")
