8556322c57cdf4462c8b89b7b9994082a3139a17
jeff <leterip@gmail.com>
2012-05-25 10:39:10 -0400

    added test that properly installes hg + bzr

--- tests/test_virtualenv_commands.go
+++ tests/test_virtualenv_commands.go
@@ -2,46 +2,69 @@ package main
 
 import (
 	"bytes"
-	"io/ioutil"
 	"log"
+	"os"
 	"os/exec"
-	fp "path/filepath"
+	"strings"
 )
 
-var (
-	dist_dir      = "/Users/zeebo/Code/pkg/goci/dist"
-	virtualenv_py = fp.Join(dist_dir, "virtualenv-1.7/virtualenv.py")
-	python        = "python2.7"
-)
-
-func virtualenv(args ...string) (cmd *exec.Cmd) {
-	args = append([]string{virtualenv_py}, args...)
-	cmd = exec.Command("python", args...)
-	return
-}
-
-func install() {
-	venv, err := ioutil.TempDir("", "venv")
+func exists(cmd string) {
+	_, err := exec.LookPath(cmd)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(cmd, " does not exist")
 	}
-
-	cmd := virtualenv("--python", python, "--distribute", "--never-download", "--prompt='(venv) '", venv)
-	var buf bytes.Buffer
-	cmd.Stdout = &buf
-	cmd.Stderr = &buf
-	if err := cmd.Run(); err != nil {
-		log.Fatal(err, buf.String())
-	}
-	log.Println(buf.String())
 }
+
+func noexists(cmd string) {
+	_, err := exec.LookPath(cmd)
+	if err == nil {
+		log.Fatal(cmd, " exists")
+	}
+}
+
+const inst_cmd = `
+	python "dist/virtualenv-1.7/virtualenv.py" --python python2.7 --distribute --never-download venv
+	. venv/bin/activate
+	pip install --use-mirrors mercurial
+	pip install --use-mirrors bzr
+`
 
 func main() {
 	//maybe have to run a shell script? :(
 	//unfortunately the activate script depends on the path we run it in
 	//perhaps we just call . bin/activate with the path set correctly
 
-	//check that hg fails
-	pth, err := exec.LookPath("hg")
-	log.Println(pth, err)
+	os.Clearenv()
+	os.Setenv("PATH", "./bin:./venv/bin")
+
+	//check that hg fails but bash and python exist
+	noexists("hg")
+	noexists("bzr")
+
+	//tools we need
+	exists("bash")
+	exists("python2.7")
+	exists("python")
+	exists("basename")
+
+	//run bash with out shell script as stdin
+	var obuf, ebuf bytes.Buffer
+	bash := exec.Command("bash")
+	bash.Stdin = strings.NewReader(inst_cmd)
+	bash.Stdout = &obuf
+	bash.Stderr = &ebuf
+	err := bash.Run()
+
+	defer os.RemoveAll("venv")
+
+	log.Println("obuf:", obuf.String())
+	log.Println("ebuf:", ebuf.String())
+	if err != nil {
+		log.Fatal("err:", err)
+	}
+
+	exists("hg")
+	exists("bzr")
+
+	log.Println("success!")
 }
