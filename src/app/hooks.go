d951400b3fdf80f6fa97ba85ce9a109c6382cdb0
jeff <leterip@me.com>
2012-05-31 19:35:33 -0400

    changed to use the payload form value

--- src/app/hooks.go
+++ src/app/hooks.go
@@ -4,13 +4,15 @@ import (
 	"github"
 	"log"
 	"net/http"
+	"strings"
 )
 
 //our basic handle index that demonstrates how to get data from the context
 //inside a template
 func handle_github_hook(w http.ResponseWriter, req *http.Request, ctx *Context) {
 	var m github.HookMessage
-	if err := m.Load(req.Body); err != nil {
+	body := strings.NewReader(req.FormValue("payload"))
+	if err := m.Load(body); err != nil {
 		log.Println("error loading hook message from github:", err)
 		perform_status(w, ctx, http.StatusInternalServerError)
 		return
