cf153fca0629c41643ab9c49582b22e329c0c7fa
jeff <leterip@me.com>
2012-05-31 10:16:07 -0400

    lots of code cleanup

--- src/builder/builder.go
+++ src/builder/builder.go
@@ -59,6 +59,13 @@ func (b build) Cleanup() (err error) {
 
 var _ Build = build{}
 
+type environ struct {
+	gopath  string
+	srcDir  string
+	tmpRepo string
+	vcs     VCS
+}
+
 func CreateBuilds(w Work) (items []Build, err error) {
 	//create a gopath to run all this stuff in
 	gopath, err := ioutil.TempDir("", "gopath")
@@ -67,94 +74,45 @@ func CreateBuilds(w Work) (items []Build, err error) {
 	}
 	defer os.RemoveAll(gopath)
 
-	if w.IsWorkspace() {
-		items, err = cloneAndTest(w, gopath, gopath)
-	} else {
-		items, err = cloneAndTest(w, gopath, fp.Join(gopath, "src", w.ImportPath()))
+	//set up an environment for the builds
+	e := environ{
+		gopath: gopath,
+		vcs:    w.VCS(),
 	}
 
+	//set the srcdir of the environment
+	if w.IsWorkspace() {
+		e.srcDir = gopath
+	} else {
+		e.srcDir = fp.Join(gopath, "src", w.ImportPath())
+	}
+
+	//grab the build items
+	items, err = createBuilds(w, e)
 	return
 }
 
-func cloneAndTest(w Work, gopath, srcDir string) (res []Build, err error) {
-	vcs := w.VCS()
-
-	tmpRepo, err := ioutil.TempDir("", "tmpRepo")
+func createBuilds(w Work, e environ) (res []Build, err error) {
+	e.tmpRepo, err = ioutil.TempDir("", "tmpRepo")
 	if err != nil {
 		return
 	}
-	defer os.RemoveAll(tmpRepo)
+	defer os.RemoveAll(e.tmpRepo)
 
 	//clone to a temporary location
-	err = vcs.Clone(w.RepoPath(), tmpRepo)
+	err = e.vcs.Clone(w.RepoPath(), e.tmpRepo)
 	if err != nil {
 		return
 	}
 
-	var packs []string
 	for _, rev := range w.Revisions() {
 		//clean bin/pkg directories from the gopath
-		os.RemoveAll(fp.Join(gopath, "pkg"))
-		os.RemoveAll(fp.Join(gopath, "bin"))
+		os.RemoveAll(fp.Join(e.gopath, "pkg"))
+		os.RemoveAll(fp.Join(e.gopath, "bin"))
 
-		bui := build{
-			rev: rev,
-		}
+		//create the build binaries for this revision
+		bui := createBuild(rev, e)
 
-		//make a new directory for the builds of this revision
-		bui.base, bui.err = ioutil.TempDir("", rev)
-		if bui.err != nil {
-			goto done
-		}
-
-		//checkout the revision we need
-		bui.err = vcs.Checkout(tmpRepo, rev)
-		if bui.err != nil {
-			goto done
-		}
-
-		//copy the repo to the srcDir
-		bui.err = copy(tmpRepo+string(fp.Separator)+".", srcDir)
-		if bui.err != nil {
-			goto done
-		}
-
-		//figure out what packages need to be built
-		packs, bui.err = list(gopath)
-		if bui.err != nil {
-			goto done
-		}
-
-		//run a get to build deps
-		bui.err = get(gopath, packs...)
-		if bui.err != nil {
-			goto done
-		}
-
-		//build the binaries and move them to a temporary directory
-		for _, pack := range packs {
-			bui.err = testbuild(gopath, pack, bui.base)
-			if bui.err != nil {
-				goto done
-			}
-
-			//what the go tool does from inspecting the source
-			_, elem := path.Split(pack)
-			name := elem + ".test" + exeSuffix
-			path := fp.Join(bui.base, name)
-
-			//make sure that binary exists before we add it to the paths. it may
-			//not exist if there are no test files, so just continue past this
-			//commit.
-			if _, err := os.Stat(path); err != nil {
-				continue
-			}
-
-			//append the path to the new dir into the paths
-			bui.paths = append(bui.paths, path)
-		}
-
-	done:
 		//if we didn't create any binaries, don't keep the dump directory around
 		if len(bui.paths) == 0 {
 			os.RemoveAll(bui.base)
@@ -162,6 +120,63 @@ func cloneAndTest(w Work, gopath, srcDir string) (res []Build, err error) {
 		}
 
 		res = append(res, bui)
+	}
+
+	return
+}
+
+func createBuild(rev string, e environ) (bui build) {
+	var packs []string
+	bui.rev = rev
+
+	//make a new directory for the builds of this revision
+	bui.base, bui.err = ioutil.TempDir("", rev)
+	if bui.err != nil {
+		return
+	}
+
+	//checkout the revision we need
+	bui.err = e.vcs.Checkout(e.tmpRepo, rev)
+	if bui.err != nil {
+		return
+	}
+
+	//copy the repo to the srcDir
+	bui.err = copy(e.tmpRepo+string(fp.Separator)+".", e.srcDir)
+	if bui.err != nil {
+		return
+	}
+
+	//figure out what packages need to be built
+	packs, bui.err = list(e.gopath)
+	if bui.err != nil {
+		return
+	}
+
+	//run a get to build deps
+	bui.err = get(e.gopath, packs...)
+	if bui.err != nil {
+		return
+	}
+
+	//build the binaries and move them to a temporary directory
+	for _, pack := range packs {
+		bui.err = testbuild(e.gopath, pack, bui.base)
+		if bui.err != nil {
+			return
+		}
+
+		//what the go tool does from inspecting the source
+		_, elem := path.Split(pack)
+		name := elem + ".test" + exeSuffix
+		path := fp.Join(bui.base, name)
+
+		//make sure that binary exists before we add it to the paths. it may
+		//not exist if there are no test files, so only add it if something
+		//is there.
+		if _, err := os.Stat(path); err == nil {
+			bui.paths = append(bui.paths, path)
+		}
 	}
 
 	return
