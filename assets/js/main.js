246272bdd3eb7ca4d93708e0b89ab3164b747071
jeff <leterip@me.com>
2012-06-20 10:52:16 -0400

    have it default closed. change the fonts up some

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,18 @@
 window.setInterval(function() {
-	$("#sidebar").load("/current/html")
-	$("#status").load("/status")
+	$("#sidebar").load("/current/html");
+	$("#status").load("/status");
 }, 10000);
 
 $(function() {
-	$("#sidebar").load("/current/html")
-	$('a.toggles').click(function() {
+	var toggle = function() {
 		$('a.toggles i').toggleClass('icon-chevron-left icon-chevron-right');
 		$('#sidebar').animate({
 			width: 'toggle'
 		}, 0);
-		$('#content').toggleClass('span12 span9');
-		$('#content').toggleClass('no-sidebar');
-	});
+		$('#content').toggleClass('span12 span9 no-sidebar');
+	};
+	$('a.toggles').click(toggle);
+	toggle();
+
+	$("#sidebar").load("/current/html");
 });
