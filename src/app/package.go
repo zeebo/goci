9ca57f03d87e46016b344fb373bb347bfd40e047
jeff <leterip@me.com>
2012-06-21 13:14:39 -0400

    have them link to gopkgdoc

--- src/app/package.go
+++ src/app/package.go
@@ -3,6 +3,7 @@ package main
 import (
 	"builder"
 	"encoding/gob"
+	"net/url"
 )
 
 type Package struct {
@@ -14,7 +15,16 @@ func (p Package) VCS() (v builder.VCS)       { return }
 func (p Package) RepoPath() (r string)       { return }
 func (p Package) ImportPath() string         { return p.Import }
 func (p Package) WorkType() builder.WorkType { return builder.WorkTypeGoinstall }
-func (p Package) Link() (l string)           { return }
+
+func (p Package) Link() (l string) {
+	u := url.URL{
+		Host:   "go.pkgdoc.org",
+		Scheme: "http",
+		Path:   "/" + p.Import,
+	}
+	l = u.String()
+	return
+}
 
 func init() {
 	gob.Register(Package{})
