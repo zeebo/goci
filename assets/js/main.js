91db19596081bb51bd5437f75db24bbc284a5b21
jake <bigjake2s@gmail.com>
2012-06-28 15:29:03 -0400

    Added JS to convert dates classed as date to the user's locale

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,13 @@ $(function() {
 
 	$("#sidebar").load("/current/html");
 });
+
+// Convert dates to user's timezone on load.
+$(function() {
+	$('.date').each(function() {
+		var dateText = $(this).text() + ' UTC'
+		var secs = Date.parse(dateText)
+		var d = new Date(secs)
+		$(this).text(d.toLocaleString())
+	})
+})
