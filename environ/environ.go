b7d52f931e5b6b3be955d38d25eaa39151a356a4
jeff <leterip@me.com>
2012-07-11 15:12:30 -0400

    docs

--- environ/environ.go
+++ environ/environ.go
@@ -7,27 +7,35 @@ import (
 	"os/exec"
 )
 
+//New returns a new environment.
 func New() (e Env) { return }
 
+//Env is the default environment that provides wrappers for os and system
+//functions with side effects.
 type Env struct{}
 
+//Exists attempts to os.Stat the name and returns if there was an error.
 func (Env) Exists(name string) bool {
 	_, err := os.Stat(name)
 	return err == nil
 }
 
+//LookPath is a wrapper for exec.LookPath
 func (Env) LookPath(name string) (string, error) {
 	return exec.LookPath(name)
 }
 
+//TempDir is a wrapper for ioutil.TempDir
 func (Env) TempDir(prefix string) (string, error) {
 	return ioutil.TempDir("", prefix)
 }
 
+//Stat is a wrapper for os.Lstat.
 func (Env) Stat(path string) (os.FileInfo, error) {
 	return os.Lstat(path)
 }
 
+//Readdir Opens the path provided and reads all the infos for the directory.
 func (Env) Readdir(path string) (s []os.FileInfo, err error) {
 	f, err := os.Open(path)
 	if err != nil {
@@ -37,18 +45,22 @@ func (Env) Readdir(path string) (s []os.FileInfo, err error) {
 	return f.Readdir(-1)
 }
 
+//Create is a wrapper for os.Create but using the specified os.FileMode
 func (Env) Create(path string, mode os.FileMode) (io.WriteCloser, error) {
 	return os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, mode)
 }
 
+//Open is a wrapper for os.Open
 func (Env) Open(path string) (io.ReadCloser, error) {
 	return os.Open(path)
 }
 
+//MkdirAll is a wrapper for os.MkdirAll
 func (Env) MkdirAll(path string, mode os.FileMode) error {
 	return os.MkdirAll(path, mode)
 }
 
+//Make creates a Proc from a Command that allows you to execute processes.
 func (Env) Make(c Command) (p Proc) {
 	cmd := &exec.Cmd{
 		Path: c.Path,
@@ -64,6 +76,7 @@ func (Env) Make(c Command) (p Proc) {
 	return procCmd{cmd}
 }
 
+//Command is a type that represents the information for executing a command.
 type Command struct {
 	W    io.Writer
 	Dir  string
@@ -72,14 +85,18 @@ type Command struct {
 	Args []string
 }
 
+//Proc is the type returned by the environments Make method that lets you run
+//the command.
 type Proc interface {
 	Run() (error, bool)
 }
 
+//procCmd is the internal representation of a process.
 type procCmd struct {
 	*exec.Cmd
 }
 
+//Run makes procCmd ad Proc and just hands it off to the wrapped exec command.
 func (p procCmd) Run() (err error, success bool) {
 	err = p.Cmd.Run()
 	success = err == nil && p.ProcessState.Success()
--- environ/mock.go
+++ environ/mock.go
@@ -5,9 +5,9 @@ import (
 	"io"
 	"math/rand"
 	"os"
-	fp "path/filepath"
 	"testing"
 	"time"
+	fp "path/filepath"
 )
 
 //
@@ -15,7 +15,6 @@ import (
 //
 
 type logger struct {
-	t      *testing.T
 	events []string
 }
 
@@ -124,6 +123,8 @@ func (t *testProc) Run() (error, bool) {
 // Test Environment World
 //
 
+//TestEnv is an environment that records all of the actions taken on it for 
+//later inspection.
 type TestEnv struct {
 	t     *logger
 	r     *rand.Rand
@@ -131,8 +132,9 @@ type TestEnv struct {
 	files map[string]io.ReadCloser
 }
 
-func NewTest(t *testing.T, seed int64) (w TestEnv) {
-	w.t = &logger{t, []string{}}
+//NewTest sets up a new testing environment with the given seed.
+func NewTest(seed int64) (w TestEnv) {
+	w.t = &logger{[]string{}}
 	w.r = rand.New(rand.NewSource(seed))
 	w.files = map[string]io.ReadCloser{}
 	return
@@ -140,40 +142,50 @@ func NewTest(t *testing.T, seed int64) (w TestEnv) {
 
 // helper functions
 
+//SetRun sets the function that Procs created by the Make method run.
 func (t *TestEnv) SetRun(r TestRun) {
 	t.run = r
 }
 
-func (w TestEnv) Events() []string {
-	return w.t.events
-}
-
-func (w TestEnv) Dump() {
-	for _, ev := range w.Events() {
-		w.t.t.Logf("%q", ev)
-	}
-}
-
+//AddFile adds the named file to the environment so that if it is opened it will
+//return the given io.ReadCloser
 func (w TestEnv) AddFile(name string, r io.ReadCloser) {
 	w.files[name] = r
 }
 
+//Events returns the list of events taken on this environment.
+func (w TestEnv) Events() []string {
+	return w.t.events
+}
+
+//Dump prints all the events this environment has seen to the testing logger.
+func (w TestEnv) Dump(t *testing.T) {
+	for _, ev := range w.Events() {
+		t.Logf("%q", ev)
+	}
+}
+
+//Reset drops all the events.
 func (w TestEnv) Reset() {
 	w.t.Reset()
 }
 
+//Logf logs the given event to the stream of events.
 func (w TestEnv) Logf(format string, vals ...interface{}) {
 	w.t.Logf(format, vals...)
 }
 
 // implement the same functionality as the default environment
 
+//Stat logs the call and returns an os.FileInfo that also logs calls.
 func (w TestEnv) Stat(name string) (os.FileInfo, error) {
 	t := w.newInfo(name)
 	w.t.Logf("world: Stat(%s): %v", name, t)
 	return t, nil
 }
 
+//Readdir logs the call and returns a random set of up to 5 os.FileInfos that
+//also log calls. It does not return the same thing for multiple calls.
 func (w TestEnv) Readdir(name string) (fi []os.FileInfo, err error) {
 	numfi := w.r.Intn(5)
 	for i := 0; i < numfi; i++ {
@@ -183,11 +195,15 @@ func (w TestEnv) Readdir(name string) (fi []os.FileInfo, err error) {
 	return
 }
 
+//Create logs the call and returns an io.WriteCloser that logs the number of
+//bytes written to it and when it is closed.
 func (w TestEnv) Create(name string, mode os.FileMode) (io.WriteCloser, error) {
 	w.t.Logf("world: Create(%s, %#o)", name, mode)
 	return testIO{w.t, name}, nil
 }
 
+//Open logs the call and returns an io.ReadCloser that logs the number of bytes
+//read from it and when it is closed.
 func (w TestEnv) Open(name string) (io.ReadCloser, error) {
 	w.t.Logf("world: Open(%s)", name)
 	if r, ok := w.files[name]; ok {
@@ -197,27 +213,33 @@ func (w TestEnv) Open(name string) (io.ReadCloser, error) {
 	return testIO{w.t, name}, nil
 }
 
+//MkdirAll logs the call.
 func (w TestEnv) MkdirAll(dir string, mode os.FileMode) error {
 	w.t.Logf("world: MkdirAll(%s, %#o)", dir, mode)
 	return nil
 }
 
+//Make logs the call and returns a Proc that runs the function set by SetRun
+//when called, logging that call.
 func (w TestEnv) Make(c Command) (p Proc) {
 	name := w.randName()
 	w.t.Logf("world: Make(): %s: %v", name, c.Args)
 	return &testProc{w.t, name, c, w.run}
 }
 
+//Exists logs the call and returns true.
 func (w TestEnv) Exists(path string) bool {
 	w.t.Logf("world: Exists(%s)", path)
 	return true
 }
 
+//LookPath logs the call and returns what was passed in with no error.
 func (w TestEnv) LookPath(path string) (string, error) {
 	w.t.Logf("world: LookPath(%s)", path)
 	return path, nil
 }
 
+//TempDir logs the call and returns a random directory with the given prefix.
 func (w TestEnv) TempDir(prefix string) (string, error) {
 	tdir := "/tmp/0" + prefix + w.randName()
 	w.t.Logf("world: TempDir(%s): %s", prefix, tdir)
@@ -236,4 +258,6 @@ func (w TestEnv) newInfo(name string) os.FileInfo {
 	return testInfo{w.t, name, w.r.Int63n(1000)}
 }
 
+//TestRun is a function that will be called when a Proc executes when added
+//to a TestEnv.
 type TestRun func(Command) (error, bool)
