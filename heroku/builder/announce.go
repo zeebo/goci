7d3b088111fb4123abd88346546c9249dd6b73e3
jeff <leterip@me.com>
2012-07-13 23:35:54 -0400

    change tracker into an rpc client

--- heroku/builder/announce.go
+++ heroku/builder/announce.go
@@ -1,6 +1,16 @@
 package main
 
-var tracker = env("TRACKER", "http://goci.me/tracker")
+import (
+	"code.google.com/p/gorilla/rpc"
+	"github.com/zeebo/goci/app/rpc/client"
+	"net/http"
+)
+
+var tracker = client.New(
+	env("TRACKER", "http://goci.me/tracker"),
+	http.DefaultClient,
+	client.JsonCodec,
+)
 
 func announce() {
 
