daa55fb0b7b2a20921e57846dbc17b99ac273b7a
jeff <leterip@gmail.com>
2012-07-16 19:26:43 -0400

    add comments to cleaner

--- heroku/builder/cleanup.go
+++ heroku/builder/cleanup.go
@@ -2,18 +2,21 @@ package main
 
 import "sync"
 
+//cleaner is a type that holds a set of functions to be run, like a defer.
 type cleaner struct {
 	in chan func()
 	fs chan []func()
 	o  *sync.Once
 }
 
+//cleanup is an instance of a cleaner.
 var cleanup = cleaner{
 	in: make(chan func()),
 	fs: make(chan []func()),
 	o:  new(sync.Once),
 }
 
+//run handles the event loop for the cleaner.
 func (c cleaner) run() {
 	funcs := make([]func(), 0)
 	for {
@@ -25,14 +28,17 @@ func (c cleaner) run() {
 	}
 }
 
+//attach takes a function and attaches it for cleanup.
 func (c cleaner) attach(f func()) {
 	c.in <- f
 }
 
+//funcs returns the set of functions to be run on cleanup.
 func (c cleaner) funcs() []func() {
 	return <-c.fs
 }
 
+//cleanup executes the cleanup of the functions once.
 func (c cleaner) cleanup() {
 	c.o.Do(func() {
 		for _, f := range c.funcs() {
