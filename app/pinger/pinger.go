70ca9d08b217c10879ecbee45564ae9c6f1d6fd4
jeff <leterip@me.com>
2012-07-15 23:35:47 -0400

    move pinger to use rpc.Nones

--- app/pinger/pinger.go
+++ app/pinger/pinger.go
@@ -1,17 +1,14 @@
 package pinger
 
-import "net/http"
+import (
+	"github.com/zeebo/goci/app/rpc"
+	"net/http"
+)
 
 //Pinger is a service that just has a Ping method to see if something exists.
 type Pinger struct{}
 
-//Ping is the argument type.
-type Ping struct{}
-
-//Pong is the reply type.
-type Pong struct{}
-
 //Ping is a simple method to see if the rpc exists.
-func (Pinger) Ping(req *http.Request, args *Ping, rep *Pong) (err error) {
+func (Pinger) Ping(req *http.Request, args *rpc.None, rep *rpc.None) (err error) {
 	return
 }
