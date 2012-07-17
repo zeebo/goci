1fe1add62dce48bde1d900a473d2cb31036be06f
jeff <leterip@me.com>
2012-07-17 10:56:22 -0400

    Removed rpc import from pinger
    This allows it to be imported both in appengine and outside of
    appengine.

--- app/pinger/pinger.go
+++ app/pinger/pinger.go
@@ -1,14 +1,14 @@
 package pinger
 
-import (
-	"github.com/zeebo/goci/app/rpc"
-	"net/http"
-)
+import "net/http"
 
 //Pinger is a service that just has a Ping method to see if something exists.
 type Pinger struct{}
 
+//None is an rpc.None inlined so we dont have the dependency.
+type None struct{}
+
 //Ping is a simple method to see if the rpc exists.
-func (Pinger) Ping(req *http.Request, args *rpc.None, rep *rpc.None) (err error) {
+func (Pinger) Ping(req *http.Request, args *None, rep *None) (err error) {
 	return
 }
