4c4f82bebaf5d6a17e09cfc9a47e392a30a03166
jeff <leterip@me.com>
2012-07-17 11:46:41 -0400

    added empty queue package

---
+++ app/queue/empty.go
@@ -0,0 +1,6 @@
+package queue
+
+//file left empty intentionally to allow goci
+//to build this package because every other
+//source file will be turned off due to the
+//dependence on appengine.
---
+++ app/queue/queue.go
@@ -0,0 +1,4 @@
+// +build !goci
+
+//package queue provides handlers for adding and dispatching work
+package queue
