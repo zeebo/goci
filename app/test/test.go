67a3dba4cb0f4f179810afd74ad6a9d71fde0a71
jeff <leterip@me.com>
2012-07-17 11:50:24 -0400

    removed add from test
    The tracker calls ping now so it won't be able to add anything.

--- app/test/test.go
+++ app/test/test.go
@@ -15,52 +15,8 @@ import (
 )
 
 func init() {
-	http.Handle("/_test/add/", http.StripPrefix("/_test/add/", httputil.Handler(add)))
 	http.Handle("/_test/lease", httputil.Handler(lease))
 	http.Handle("/_test/ping", httputil.Handler(ping))
-}
-
-func parse(path string, args ...*string) {
-	parts := strings.Split(path, "/")
-	min := len(parts)
-	if len(args) < min {
-		min = len(args)
-	}
-	for i := 0; i < min; i++ {
-		*args[i] = parts[i]
-	}
-	return
-}
-
-func add(w http.ResponseWriter, req *http.Request, ctx appengine.Context) (e *httputil.Error) {
-	var Type, GOOS, GOARCH string
-	parse(req.URL.Path, &Type, &GOOS, &GOARCH)
-	if GOOS == "" {
-		GOOS = runtime.GOOS
-	}
-	if GOARCH == "" {
-		GOARCH = runtime.GOARCH
-	}
-
-	//set up the request
-	args := &rpc.AnnounceArgs{
-		GOOS:   GOOS,
-		GOARCH: GOARCH,
-		Type:   Type,
-		URL:    fmt.Sprintf("foo%d", rand.Int()),
-	}
-	var reply rpc.AnnounceReply
-	ctx.Infof("%+v", args)
-
-	//perform the call
-	if err := tracker.DefaultTracker.Announce(req, args, &reply); err != nil {
-		e = httputil.Errorf(err, "error calling announce")
-		return
-	}
-	//log the reply
-	ctx.Infof("%+v", reply)
-	fmt.Fprintf(w, "%+v\n", reply)
-	return
 }
 
 func lease(w http.ResponseWriter, req *http.Request, ctx appengine.Context) (e *httputil.Error) {
