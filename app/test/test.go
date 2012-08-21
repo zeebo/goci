2aaf4c9949a76093865984bda8daf668db2d7f85
jeff <leterip@me.com>
2012-08-21 12:59:10 -0400

    convert test off appengine

--- app/test/test.go
+++ app/test/test.go
@@ -3,22 +3,20 @@
 package test
 
 import (
-	"appengine"
 	"fmt"
-	"httputil"
+	"github.com/zeebo/goci/app/httputil"
+	// "github.com/zeebo/goci/app/queue"
+	// "github.com/zeebo/goci/app/rpc"
+	"github.com/zeebo/goci/app/tracker"
 	"net/http"
-	"queue"
-	"rpc"
-	"tracker"
 )
 
 func init() {
 	http.Handle("/_test/lease", httputil.Handler(lease))
-	http.Handle("/_test/ping", httputil.Handler(ping))
-	http.Handle("/_test/addwork", httputil.Handler(addwork))
+	// http.Handle("/_test/addwork", httputil.Handler(addwork))
 }
 
-func lease(w http.ResponseWriter, req *http.Request, ctx appengine.Context) (e *httputil.Error) {
+func lease(w http.ResponseWriter, req *http.Request, ctx httputil.Context) (e *httputil.Error) {
 	bk, rk, b, r, err := tracker.LeasePair(ctx)
 	if err != nil {
 		e = httputil.Errorf(err, "error leasing pair")
@@ -29,33 +27,24 @@ func lease(w http.ResponseWriter, req *http.Request, ctx appengine.Context) (e *
 	return
 }
 
-func ping(w http.ResponseWriter, req *http.Request, ctx appengine.Context) (e *httputil.Error) {
-	if err := tracker.DefaultTracker.Ping(req, nil, nil); err != nil {
-		e = httputil.Errorf(err, "error sending ping")
-		return
-	}
-	fmt.Fprintf(w, "ping!")
-	return
-}
+// func addwork(w http.ResponseWriter, req *http.Request, ctx appengine.Context) (e *httputil.Error) {
+// 	//create our little work item
+// 	q := rpc.Work{
+// 		Revision:   "e9dd26552f10d390b5f9f59c6a9cfdc30ed1431c",
+// 		ImportPath: "github.com/zeebo/irc",
+// 	}
 
-func addwork(w http.ResponseWriter, req *http.Request, ctx appengine.Context) (e *httputil.Error) {
-	//create our little work item
-	q := rpc.Work{
-		Revision:   "e9dd26552f10d390b5f9f59c6a9cfdc30ed1431c",
-		ImportPath: "github.com/zeebo/irc",
-	}
+// 	q = rpc.Work{
+// 		Revision:    "7e283bf6dbf4c97a00647f873faa0b513ad59fbf",
+// 		ImportPath:  "github.com/zeebo/goci",
+// 		Subpackages: true,
+// 	}
 
-	q = rpc.Work{
-		Revision:    "7e283bf6dbf4c97a00647f873faa0b513ad59fbf",
-		ImportPath:  "github.com/zeebo/goci",
-		Subpackages: true,
-	}
+// 	//add it to the queue
+// 	if err := queue.QueueWork(ctx, q); err != nil {
+// 		e = httputil.Errorf(err, "error adding work item to queue")
+// 		return
+// 	}
 
-	//add it to the queue
-	if err := queue.QueueWork(ctx, q); err != nil {
-		e = httputil.Errorf(err, "error adding work item to queue")
-		return
-	}
-
-	return
-}
+// 	return
+// }
