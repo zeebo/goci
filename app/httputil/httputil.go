cd3233520fd0022918f83bb2ce3de6ef75527801
jeff <leterip@me.com>
2012-08-21 12:57:38 -0400

    convert httputil off of appengine

--- app/httputil/httputil.go
+++ app/httputil/httputil.go
@@ -4,20 +4,59 @@
 package httputil
 
 import (
-	"appengine"
-	"appengine/datastore"
-	"encoding/json"
 	"fmt"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 	"net/http"
 )
 
-type Handler func(http.ResponseWriter, *http.Request, appengine.Context) *Error
+var baseDb *mgo.Database
+
+func Init(db *mgo.Database) {
+	baseDb = db
+}
+
+func NewContext(req *http.Request) Context {
+	if baseDb == nil {
+		panic("httputil.Init not called before creating contexts")
+	}
+
+	return Context{
+		DB: baseDb.Session.Clone().DB(baseDb.Name),
+	}
+}
+
+type Context struct {
+	DB *mgo.Database
+}
+
+func (c *Context) close() {
+	c.DB.Session.Close()
+}
+
+func (c *Context) logf(severity, format string, items ...interface{}) {
+	c.DB.C("logs").Insert(bson.M{
+		"severity": severity,
+		"text":     fmt.Sprintf(format, items...),
+	})
+}
+
+func (c *Context) Errorf(format string, items ...interface{}) {
+	c.logf("error", format, items...)
+}
+
+func (c *Context) Infof(format string, items ...interface{}) {
+	c.logf("info", format, items...)
+}
+
+type Handler func(http.ResponseWriter, *http.Request, Context) *Error
 
 func (fn Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
+	c := NewContext(r)
+	defer c.close()
 	if e := fn(w, r, c); e != nil {
 		http.Error(w, e.Message, e.Code)
-		c.Errorf("%s (%v)", e.Message, e.Error)
+		c.Errorf("[%d] %s (%v)", e.Code, e.Message, e.Error)
 	}
 }
 
@@ -29,25 +68,4 @@ type Error struct {
 
 func Errorf(err error, format string, v ...interface{}) *Error {
 	return &Error{err, fmt.Sprintf(format, v...), 500}
-}
-
-func ToString(key *datastore.Key) (s string) {
-	b, err := json.Marshal(key)
-	if err != nil {
-		panic(err)
-	}
-	//remove the `"` surrounding the string to avoid double marshal
-	return string(b[1 : len(b)-1])
-}
-
-func FromString(key string) *datastore.Key {
-	k := new(datastore.Key)
-	b, err := json.Marshal(key)
-	if err != nil {
-		panic(err)
-	}
-	if err := json.Unmarshal(b, k); err != nil {
-		panic(err)
-	}
-	return k
 }
