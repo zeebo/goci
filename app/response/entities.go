1f05ccb2b1e90bb05f28ff57b6e68040662ec279
jeff <leterip@me.com>
2012-08-21 13:15:14 -0400

    convert response off appengine

--- app/response/entities.go
+++ app/response/entities.go
@@ -1,14 +1,20 @@
 package response
 
-import "time"
+import (
+	"labix.org/v2/mgo/bson"
+	"time"
+)
 
 //TestResult is an entity type that describes the sucessful running of a test
 //and the output it generated.
 type TestResult struct {
+	ID           bson.ObjectId `bson:"_id,omitempty"`
+	WorkResultID bson.ObjectId //key of the work result that this came from
+
 	ImportPath string    //import path of the test
 	Revision   string    //revision of the source code
 	RevDate    time.Time //when the revision was commit
-	When       time.Time //when the test was run
+	When       time.Time //when the test result was recorded
 	Output     []byte    //the output of the test
 	Status     string    //the status of the build: (Pass/Fail/WontBuild/Error)
 }
@@ -17,6 +23,9 @@ type TestResult struct {
 //run through the queue. It records any build failures or other errors in
 //generating the test results.
 type WorkResult struct {
+	ID     bson.ObjectId `bson:"_id,omitempty"`
+	WorkID bson.ObjectId //id of the work item that generated this result
+
 	Success  bool      //all the work items completed sucessfully
 	Revision string    //the revision of the build (if known)
 	RevDate  time.Time //when the revision was commit (if known)
--- app/response/response.go
+++ app/response/response.go
@@ -4,15 +4,14 @@
 package response
 
 import (
-	"appengine"
-	"appengine/datastore"
 	gorcp "code.google.com/p/gorilla/rpc"
 	gojson "code.google.com/p/gorilla/rpc/json"
 	"fmt"
-	"httputil"
+	"github.com/zeebo/goci/app/httputil"
 	"net/http"
-	"queue"
-	"rpc"
+	// "queue"
+	"github.com/zeebo/goci/app/rpc"
+	"labix.org/v2/mgo/bson"
 	"strings"
 	"time"
 )
@@ -37,90 +36,89 @@ func (Response) Post(req *http.Request, args *rpc.RunnerResponse, resp *rpc.None
 	defer rpc.Wrap(&err)
 
 	//create our context
-	ctx := appengine.NewContext(req)
+	ctx := httputil.NewContext(req)
 	ctx.Infof("Storing runner result")
 	ctx.Infof("%+v", args)
 
-	//make sure the TaskInfo for this request still exists, and if so, remove it
-	found, err := queue.FindTaskInfo(ctx, args.ID)
+	//TODO(zeebo): search for the TaskInfo from the queue
+	/*
+		//make sure the TaskInfo for this request still exists, and if so, remove it
+		found, err := queue.FindTaskInfo(ctx, args.ID)
 
-	if err != nil {
-		ctx.Errorf("error finding task info: %v", err)
-		return
-	}
-
-	//if we didn't find our TaskInfo, just bail
-	if !found {
-		ctx.Errorf("Got a late response")
-		return
-	}
-
-	//get the key of the work item
-	key := httputil.FromString(args.Key)
-
-	trans := func(ctx appengine.Context) (err error) {
-		//create a WorkResult
-		w := &WorkResult{
-			Success:  true,
-			Revision: args.Revision,
-			RevDate:  args.RevDate,
-			When:     time.Now(),
-		}
-
-		//store it
-		wkey := datastore.NewIncompleteKey(ctx, "WorkResult", key)
-		if wkey, err = datastore.Put(ctx, wkey, w); err != nil {
-			ctx.Errorf("Error storing WorkResult: %v", err)
+		if err != nil {
+			ctx.Errorf("error finding task info: %v", err)
 			return
 		}
 
-		//store the test results
-		for _, out := range args.Tests {
+		//if we didn't find our TaskInfo, just bail
+		if !found {
+			ctx.Errorf("Got a late response")
+			return
+		}
+	*/
 
-			//get the status from the output type and output
-			var status string
-			switch out.Type {
-			case rpc.OutputSuccess:
-				if strings.HasSuffix(out.Output, "\nPASS\n") {
-					status = "Pass"
-				} else {
-					status = "Fail"
-				}
-			case rpc.OutputWontBuild:
-				status = "WontBuild"
-			case rpc.OutputError:
-				status = "Error"
-			default:
-				err = fmt.Errorf("unknown output type: %s", out.Type)
-				return
-			}
+	//TODO(zeebo): put this in a transaction to skip partial test results
 
-			//make a TestResult
-			t := &TestResult{
-				ImportPath: out.ImportPath,
-				Revision:   args.Revision,
-				RevDate:    args.RevDate,
-				When:       time.Now(),
-				Output:     []byte(out.Output),
-				Status:     status,
-			}
+	//get the key of the work item
+	key := bson.ObjectIdHex(args.Key)
 
-			//store it
-			tkey := datastore.NewIncompleteKey(ctx, "TestResult", wkey)
-			if _, err = datastore.Put(ctx, tkey, t); err != nil {
-				ctx.Errorf("Error storing TestResult: %v", err)
-				return
+	//create a WorkResult
+	wkey := bson.NewObjectId()
+	w := &WorkResult{
+		ID:       wkey,
+		WorkID:   key,
+		Success:  true,
+		Revision: args.Revision,
+		RevDate:  args.RevDate,
+		When:     time.Now(),
+	}
+
+	//store it
+	if err = ctx.DB.C("WorkResult").Insert(w); err != nil {
+		ctx.Errorf("Error storing WorkResult: %v", err)
+		return
+	}
+
+	//store the test results
+	for _, out := range args.Tests {
+
+		//get the status from the output type and output
+		var status string
+		switch out.Type {
+		case rpc.OutputSuccess:
+			if strings.HasSuffix(out.Output, "\nPASS\n") {
+				status = "Pass"
+			} else {
+				status = "Fail"
 			}
+		case rpc.OutputWontBuild:
+			status = "WontBuild"
+		case rpc.OutputError:
+			status = "Error"
+		default:
+			err = fmt.Errorf("unknown output type: %s", out.Type)
+			return
 		}
 
-		return
+		//make a TestResult
+		t := &TestResult{
+			ID:           bson.NewObjectId(),
+			WorkResultID: wkey,
+			ImportPath:   out.ImportPath,
+			Revision:     args.Revision,
+			RevDate:      args.RevDate,
+			When:         time.Now(),
+			Output:       []byte(out.Output),
+			Status:       status,
+		}
+
+		//store it
+		if err = ctx.DB.C("TestResult").Insert(t); err != nil {
+			ctx.Errorf("Error storing TestResult: %v", err)
+			return
+		}
 	}
 
-	if err = datastore.RunInTransaction(ctx, trans, nil); err != nil {
-		return
-	}
-
-	//we did it!
 	return
 }
 
@@ -130,30 +128,35 @@ func (Response) Error(req *http.Request, args *rpc.BuilderResponse, resp *rpc.No
 	defer rpc.Wrap(&err)
 
 	//create the context
-	ctx := appengine.NewContext(req)
+	ctx := httputil.NewContext(req)
 	ctx.Infof("Storing a builder error")
 	ctx.Infof("%+v", args)
 
-	//make sure the TaskInfo for this request still exists, and if so, remove it
-	found, err := queue.FindTaskInfo(ctx, args.ID)
+	//TODO(zeebo): search for the task info from the queue
+	/*
+		//make sure the TaskInfo for this request still exists, and if so, remove it
+		found, err := queue.FindTaskInfo(ctx, args.ID)
 
-	//make sure we check the error
-	if err != nil {
-		ctx.Errorf("error finding task info: %v", err)
-		return
-	}
+		//make sure we check the error
+		if err != nil {
+			ctx.Errorf("error finding task info: %v", err)
+			return
+		}
 
-	//if we didn't find it, just bail
-	if !found {
-		ctx.Errorf("Got a late response")
-		return
-	}
+		//if we didn't find it, just bail
+		if !found {
+			ctx.Errorf("Got a late response")
+			return
+		}
+	*/
 
 	//get the key of the work item
-	key := httputil.FromString(args.Key)
+	key := bson.ObjectIdHex(args.Key)
 
 	//create a WorkResult
 	w := &WorkResult{
+		ID:       bson.NewObjectId(),
+		WorkID:   key,
 		Success:  false,
 		When:     time.Now(),
 		Revision: args.Revision,
@@ -162,8 +165,7 @@ func (Response) Error(req *http.Request, args *rpc.BuilderResponse, resp *rpc.No
 	}
 
 	//store it
-	wkey := datastore.NewIncompleteKey(ctx, "WorkResult", key)
-	if _, err = datastore.Put(ctx, wkey, w); err != nil {
+	if err = ctx.DB.C("WorkResult").Insert(w); err != nil {
 		ctx.Errorf("Error storing WorkResult: %v", err)
 		return
 	}
